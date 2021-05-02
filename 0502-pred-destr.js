93361a77969b4a4355c6a29eb686312eae4f5ff1
Dirk Kinley <42229326+dkinley@users.noreply.github.com>
2021-05-02 14:45:27 -0700

    Add files via upload

---
+++ 0502-pred-destr.js
@@ -0,0 +1,43 @@
+
+// //1 predicted Tesla followed by Mercedes
+// const cars = ['Tesla', 'Mercedes', 'Honda']
+// const [ randomCar ] = cars
+// const [ ,otherRandomCar ] = cars
+// //Predict the output
+// console.log(randomCar)
+// console.log(otherRandomCar)
+
+// //2 predicted error as 'name' is not available globally to log
+// const employee = {
+//     name: 'Elon',
+//     age: 47,
+//     company: 'Tesla'
+// }
+// const { name: otherName } = employee;
+// //Predict the output
+// console.log(name);
+// console.log(otherName);
+
+//3 predicting 12345 first then an error b/c the const password is not in the person object 
+// const person = {
+//     name: 'Phil Smith',
+//     age: 47,
+//     height: '6 feet'
+// }
+// const password = '12345';
+// const { password: hashedPassword } = person;  
+// //Predict the output
+// console.log(password);
+// console.log(hashedPassword);
+
+//4 Predicted false, true
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);
+console.log(first == third);
+console.log(first);
+console.log(second);
+console.log(third);
