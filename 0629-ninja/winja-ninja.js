cbd8b972ded4dbfdb0e9aa2b7283f47a3759e54c
Dirk Kinley <dkinley@gmail.com>
2021-06-29 21:03:40 -0700

    winja is the wiining nija

---
+++ 0629-ninja/winja-ninja.js
@@ -0,0 +1,34 @@
+//this is creating a Ninja class
+
+class ninja {
+    const (name, health, speed = 3, strength = 3){
+        this.name = name;
+        this.health = health;
+        this.speed = speed;
+        this.strength = strength;   
+    }
+    sayName(){
+        console.log("Ninjas name is: " + this.name);
+    }
+    showStats(){
+        console.log("Stats for: " + this.name);
+        console.log("Health: " + this.health);
+        console.log("Strength: " + this.strength);
+        console.log("Speed: " + this.speed);
+        
+    }
+    drinkSake(){
+        this.health += 10;
+    }
+}
+    const ninjaOne = new ninja("First One");
+    ninjaOne.sayName();
+    ninjaOne.showStats();
+    ninjaOne.drinkSake();
+
+    console.log("Ninja Dos");
+    const ninjaTwo = new ninja("Second One");
+    ninjaTwo.sayName();
+    ninjaTwo.showStats();
+    ninjaTwo.drinkSake();
+ 
