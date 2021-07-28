5c311e147f2798763dfce1cf453cce64e86dc71a
Dirk Kinley <dkinley@gmail.com>
2021-07-27 18:44:03 -0700

    Updates to components for axios calls

---
+++ d2w-logistics-main/client/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import Login from './components/Login';
+import RegisterUser from './components/RegisterUser';
+import GetAll from './components/GetAll';
+import Create from './components/Create';
+import Edit from './components/Edit';
+import Details from './components/Details';
+import { Router } from '@reach/router'; // since router is not exported as default {} are required
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <h1>Down To The Wire Logistics</h1>
+      <Router>
+        <Login default/>
+        <RegisterUser path="/signup"/>
+        <GetAll path="/capacities" />
+        <Create path="/capacities/create" />
+        <Edit path="/capacities/:id/edit" />
+        <Details path="/capacities/:id"/> 
+      </Router>
+    </div>
+  );
+}
+export default App;
